5cee09065b7258b7054e62dc94eae6e43790b44c
Aarush Bayyana <127845895+Poltergeist1225@users.noreply.github.com>
2023-11-05 08:33:24 +0530

    Add files via upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV !== 'production') {
   })
   
   const users = []
+  const data = []
   
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
@@ -47,7 +48,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(methodOverride('_method'))
   
   app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
+    res.render('index.ejs', { firstname: req.user.firstname, lastname: req.user.lastname })
   })
   
   app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -69,12 +70,13 @@ if (process.env.NODE_ENV !== 'production') {
       const hashedPassword = await bcrypt.hash(req.body.password, 10)
       users.push({
         id: Date.now().toString(),
-        name: req.body.name,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
         email: req.body.email,
         password: hashedPassword
       })
       res.redirect('/login')
-    } catch {
+    }catch {
       res.redirect('/register')
     }
   })
@@ -85,8 +87,125 @@ if (process.env.NODE_ENV !== 'production') {
             return next(err)
         }
     })
-    res.redirect('/login')
+    res.redirect('/start')
   })
+
+//Start Page
+
+  app.get('/start', (req,res) => {
+    res.render('start.ejs')
+  })
+
+  app.post('/start', (req,res) => {
+    try {
+      
+      res.redirect('/register')
+    } catch {
+      res.redirect('/start')
+    }
+  })
+
+//Profile
+
+  app.get('/profile', (req, res) => {
+    res.render('profile.ejs')
+  })
+
+  app.post('/profile', checkNotAuthenticated, (req,res) => {
+    try {
+      data.push({
+        id: Date.now().toString(),
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        allergies: req.body.allergies,
+        chronic_conditions: req.body.chronic_conditions,
+        medications: req.body.medications
+      })
+      console.log(req.body.gender)
+      res.redirect('/')
+    } catch {
+      res.redirect('/profile', { firstname: req.user.firstname, lastname: req.user.lastname })
+    }
+  })
+
+  //Upload
+
+  app.get('/upload', (req, res) => {
+    res.render('upload.ejs')
+  })
+
+  app.post('/upload', checkNotAuthenticated, (req,res) => {
+    try {
+      
+      res.redirect('/')
+    } catch {
+      res.redirect('/profile', { firstname: req.user.firstname, lastname: req.user.lastname })
+    }
+  })
+
+  //Blood Sugar
+
+  app.get('/blood_sugar', (req, res) => {
+    res.render('blood_sugar.ejs')
+  })
+
+  app.post('/blood_sugar', checkNotAuthenticated, (req,res) => {
+    try {
+      
+      res.redirect('/blood_sugar')
+    } catch {
+      res.redirect('/blood_sugar')
+    }
+  })
+
+  //Analytics
+
+  app.get('/analytics', (req, res) => {
+    res.render('analytics.ejs')
+  })
+
+  app.post('/analytics', checkNotAuthenticated, (req,res) => {
+    try {
+      
+      res.redirect('/analytics')
+    } catch {
+      res.redirect('/analytics')
+    }
+  })
+
+  //Graph
+
+  app.get('/graph', (req, res) => {
+    res.render('graph.ejs')
+  })
+
+  app.post('/profile', checkNotAuthenticated, (req,res) => {
+    try {
+      
+      res.redirect('/graph')
+    } catch {
+      res.redirect('/graph')
+    }
+  })
+
+  //Profile View
+
+  app.get('/view_profile', (req, res) => {
+    res.render('view_profile.ejs')
+  })
+
+  app.post('/view_profile', checkNotAuthenticated, (req,res) => {
+    try {
+      
+      res.redirect('/')
+    } catch {
+      res.redirect('/view_profile')
+    }
+  })
+
+  //Check Auth
   
   function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
